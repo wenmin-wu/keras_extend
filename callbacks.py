d2bf50cfa04fda9600f755ad90b0ba24d8cc5af4
wuwenmin-sal <wuwenmin-sal@360.cn>
2018-03-09 18:24:28 +0800

    add a BatchTensorBoard

--- callbacks.py
+++ callbacks.py
@@ -1,3 +1,4 @@
+import tensorflow as tf
 from keras.callbacks import Callback
 from keras.callbacks import TensorBoard
 
@@ -33,4 +34,44 @@ class NBatchLogger(Callback):
             self.metric_cache.clear()
 
 class BatchTensorBoard(TensorBoard):
-    pass
+    """
+    add summaries to TensorBoard on batch end.
+    """
+    def __init__(self, log_dir='./logs',
+                 histogram_freq=0,
+                 batch_size=32,
+                 write_graph=True,
+                 write_grads=False,
+                 write_images=False,
+                 embeddings_freq=0,
+                 embeddings_layer_names=None,
+                 embeddings_metadata=None):
+        super(BatchTensorBoard, self).__init__(log_dir,
+                                               histogram_freq,
+                                               batch_size,
+                                               write_graph,
+                                               write_grads,
+                                               write_images,
+                                               embeddings_freq,
+                                               embeddings_layer_names,
+                                               embeddings_metadata)
+        self.seen = 0
+
+    #TODO: find a better to deal with this
+    def on_epoch_end(self, epoch, logs=None):
+        """
+        to avoid the summary being overrite
+        """
+        pass
+
+    def on_batch_end(self, batch, logs={}):
+        for name, value in logs.items():
+            if name == 'batch' or name == 'size':
+                continue
+            summary = tf.Summary()
+            summary_value = summary.value.add()
+            summary_value.simple_value = value.item()
+            summary_value.tag = name
+            self.writer.add_summary(summary, self.seen)
+        self.writer.flush()
+        self.seen += self.batch_size
